2bf4416319badae4d98ab89598ee1b2c0960fe3c
Samuele Catalano <csamuele73@mail.com>
2022-10-11 14:52:23 +0200

    Swagger commit

---
+++ src/main/java/Swagger01/swagger/SpringFoxConfig.java
@@ -0,0 +1,88 @@
+package Swagger01.swagger;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SpringFoxConfig {
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfo(
+                        "Swagger API",
+                        "Lesson Swagger Generation",
+                        "1.0.0",
+                        "https://en.wikipedia.org/wiki/MIT_License                                                                          ",
+                        new Contact("Samuele", "https://develhope.co","csamuele73@gmail.com"),
+                        "GNU",
+                        "https://www.gnu.org/licenses/gpl-3.0.en.html",
+                        Collections.emptyList()
+                ))
+                .useDefaultResponseMessages(false)
+                .tags(
+                        new Tag("hello-controller", "The hello controllers"),
+                        new Tag("echo-controller", "The echo controllers")
+                );
+
+    }
+
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+}
+
+
+
+
--- src/main/java/Swagger01/swagger/Swagger01Application.java
+++ src/main/java/Swagger01/swagger/Swagger01Application.java
@@ -2,8 +2,10 @@ package Swagger01.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Swagger01Application {
 
 	public static void main(String[] args) {
